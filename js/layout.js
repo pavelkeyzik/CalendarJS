828264435b29d80c334e6ded1230a63d61a7fcd8
Pavel Keyzik <pavel.keyzik@gmail.com>
2017-12-28 20:27:20 +0300

    App init

---
+++ js/layout.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  var body = document.body;
+  var text = document.createElement('p');
+  text.innerText = 'Calendar app';
+  body.insertBefore(text, body.childNodes[0]);
+})();
