e6f7ec8b0afe1b775e9f0c1f910331cff7b17f6a
Pavel Keyzik <pavel.keyzik@gmail.com>
2017-12-31 19:49:48 +0300

    Calendar template in JS and added OOP

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
-(function() {
   // -> Start Loading modules
 
-  var calendar = window.calendarModule();
+  // var calendar = window.calendarModule();
+
 
   // <- End Loading modules
 
-  var currentLayout = document.getElementsByClassName('js-calendar__current')[0];
-  currentLayout.innerText = calendar.getCurrentDay();
+  var calendar = new Calendar('firstCalendar', 'short');
+  // document.getElementById('firstCalendar').innerHTML = calendar.getTemplate();
 
-  var prevMonth = document.getElementsByClassName('js-month__prev')[0];
-  var nextMonth = document.getElementsByClassName('js-month__next')[0];
+  // var currentLayout = document.getElementsByClassName('js-calendar__current')[0];
+  // currentLayout.innerText = calendar.getCurrentDay();
 
-  prevMonth.addEventListener('click', function() {
-    calendar.monthPrev();
-    currentLayout.innerText = calendar.getCurrentDay();
-  });
+  // var prevMonth = document.getElementsByClassName('js-month__prev')[0];
+  // var nextMonth = document.getElementsByClassName('js-month__next')[0];
 
-  nextMonth.addEventListener('click', function() {
-    calendar.monthNext();
-    currentLayout.innerText = calendar.getCurrentDay();
-  });
+  // prevMonth.addEventListener('click', function() {
+    // calendar.monthPrev();
+    // currentLayout.innerText = calendar.getCurrentDay();
+  // });
 
-
-})();
+  // nextMonth.addEventListener('click', function() {
+    // calendar.monthNext();
+    // currentLayout.innerText = calendar.getCurrentDay();
+  // });
