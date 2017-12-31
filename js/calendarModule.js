7680e4085d61eaac8502c1b449177f7257f16c67
Pavel Keyzik <pavel.keyzik@gmail.com>
2017-12-31 19:49:48 +0300

    Calendar template in JS and added OOP

--- js/calendarModule.js
+++ js/calendarModule.js
@@ -1,60 +1,143 @@
 'use strict';
 
-(function(){
+var monthNameShort = [
+  'Jan', 'Feb', 'Mar',
+  'Apr', 'May', 'June',
+  'July', 'Aug', 'Sept',
+  'Oct', 'Nov', 'Dec'
+];
 
-  var monthNameShort = [
-    'Jan', 'Feb', 'Mar',
-    'Apr', 'May', 'June',
-    'July', 'Aug', 'Sept',
-    'Oct', 'Nov', 'Dec'
-  ];
+var monthNameLong = [
+  'January', 'February', 'March',
+  'April', 'May', 'June',
+  'July', 'August', 'September',
+  'October', 'November', 'December'
+];
 
-  var monthNameLong = [
-    'January', 'February', 'March',
-    'April', 'May', 'June',
-    'July', 'August', 'September',
-    'October', 'November', 'December'
-  ];
+function Calendar(id, type) {
+  var this_  = this;
 
-  function calendarModule() {
-    var vm = this;
-    var date = new Date();
-    var selectedMonth = date.getMonth();
-    var selectedYear = date.getFullYear();
+  if(type == 'short') this_.monthName = monthNameShort;
+  else if(type == 'long') this_.monthName = monthNameLong;
 
-    function getCurrentDay() {
-      return monthNameLong[selectedMonth] + ' ' + selectedYear;
-    }
+  var date = new Date();
+  var selectedMonth = date.getMonth();
+  var selectedYear = date.getFullYear();
 
-    function monthNext() {
-      if(selectedMonth < 11) {
-        selectedMonth++;
-      } else {
-        selectedMonth = 0;
-        selectedYear++;
-      }
-    }
-
-    function monthPrev() {
-      if(selectedMonth > 0) {
-        selectedMonth--;
-      } else {
-        selectedMonth = 11;
-        selectedYear--;
-      }
-    }
-
-    function getSelectedMonth() {
-      return selectedMonth;
-    }
-
-    return {
-      getCurrentDay: getCurrentDay,
-      monthNext: monthNext,
-      monthPrev: monthPrev,
-      getSelectedMonth: getSelectedMonth
-    }
+  this.getCurrentDay = function() {
+    return this_.monthName[selectedMonth] + ' ' + selectedYear;
   }
 
-  window.calendarModule = calendarModule;
-})();
+  this.calendarPromise = new Promise(function(resolve, reject) {
+    resolve(this_.getCurrentDay());
+  });
+
+  this.monthPrev = function() {
+    if(selectedMonth > 0) {
+      selectedMonth--;
+    } else {
+      selectedMonth = 11;
+      selectedYear--;
+    }
+    this_.updateTemplate();
+  }
+
+  this.monthNext = function() {
+    if(selectedMonth < 11) {
+      selectedMonth++;
+    } else {
+      selectedMonth = 0;
+      selectedYear++;
+    }
+    this_.updateTemplate();
+  }
+
+  this_.updateTemplate = function() {
+    document.getElementById(id).innerHTML = this_.getTemplate();
+  }
+
+  this_.getTemplate = function() {
+    return `<div class="calendar">
+      <div class="calendar__action">
+        <div class="calendar__left js-month__prev" onclick="calendar.monthPrev();">
+          <i class="material-icons">chevron_left</i>
+        </div>
+        <div class="calendar__current js-calendar__current">
+          ${this.getCurrentDay()}
+        </div>
+        <div class="calendar__right js-month__next" onclick="calendar.monthNext();">
+          <i class="material-icons">chevron_right</i>
+        </div>
+      </div>
+      <div class="calendar__content">
+        <ul class="calendar__days">
+          <li>Sun</li>
+          <li>Mon</li>
+          <li>Tue</li>
+          <li>Wed</li>
+          <li>Thu</li>
+          <li>Fri</li>
+          <li>Sat</li>
+        </ul>
+        <div class="calendar__numbers">
+          <div class="row">
+            <li class="calendar__numbers--inactive">26</li>
+            <li class="calendar__numbers--inactive">27</li>
+            <li class="calendar__numbers--inactive">28</li>
+            <li class="calendar__numbers--inactive">29</li>
+            <li class="calendar__numbers--inactive">30</li>
+            <li class="calendar__numbers--active">1</li>
+            <li class="calendar__numbers--active">2</li>
+          </div>
+          <div class="row">
+            <li class="calendar__numbers--active">3</li>
+            <li class="calendar__numbers--active">4</li>
+            <li class="calendar__numbers--active">5</li>
+            <li class="calendar__numbers--active">6</li>
+            <li class="calendar__numbers--active">7</li>
+            <li class="calendar__numbers--active">8</li>
+            <li class="calendar__numbers--active">9</li>
+          </div>
+          <div class="row">
+            <li class="calendar__numbers--active">10</li>
+            <li class="calendar__numbers--active">11</li>
+            <li class="calendar__numbers--active">12</li>
+            <li class="calendar__numbers--active">13</li>
+            <li class="calendar__numbers--active">14</li>
+            <li class="calendar__numbers--active">15</li>
+            <li class="calendar__numbers--active">16</li>
+          </div>
+          <div class="row">
+            <li class="calendar__numbers--active">17</li>
+            <li class="calendar__numbers--active">18</li>
+            <li class="calendar__numbers--active">19</li>
+            <li class="calendar__numbers--active">20</li>
+            <li class="calendar__numbers--active">21</li>
+            <li class="calendar__numbers--active">22</li>
+            <li class="calendar__numbers--active">23</li>
+          </div>
+          <div class="row">
+            <li class="calendar__numbers--active">24</li>
+            <li class="calendar__numbers--active">25</li>
+            <li class="calendar__numbers--active">26</li>
+            <li class="calendar__numbers--active">27</li>
+            <li class="calendar__numbers--active">28</li>
+            <li class="calendar__numbers--active">29</li>
+            <li class="calendar__numbers--active">30</li>
+          </div>
+          <div class="row">
+            <li class="calendar__numbers--active">31</li>
+            <li class="calendar__numbers--inactive">1</li>
+            <li class="calendar__numbers--inactive">2</li>
+            <li class="calendar__numbers--inactive">3</li>
+            <li class="calendar__numbers--inactive">4</li>
+            <li class="calendar__numbers--inactive">5</li>
+            <li class="calendar__numbers--inactive">6</li>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  }
+
+  this_.updateTemplate();
+}
--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
 'use strict';
 
-(function() {
-  // -> Start Loading modules
-
-  var calendar = window.calendarModule();
-
-  // <- End Loading modules
-
-  var currentLayout = document.getElementsByClassName('js-calendar__current')[0];
-  currentLayout.innerText = calendar.getCurrentDay();
-
-  var prevMonth = document.getElementsByClassName('js-month__prev')[0];
-  var nextMonth = document.getElementsByClassName('js-month__next')[0];
-
-  prevMonth.addEventListener('click', function() {
-    calendar.monthPrev();
-    currentLayout.innerText = calendar.getCurrentDay();
-  });
-
-  nextMonth.addEventListener('click', function() {
-    calendar.monthNext();
-    currentLayout.innerText = calendar.getCurrentDay();
-  });
-
-
-})();
+var calendar = new Calendar('firstCalendar', 'short');
